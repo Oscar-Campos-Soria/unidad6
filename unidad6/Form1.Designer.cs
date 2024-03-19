46f23e47573885601f750eaf7deeeaf5f358d960
FALKON <campossoriao1@gmail.com>
2024-03-19 06:38:52 +0100

    Agregar archivos de proyecto.

---
+++ unidad6/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace unidad6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEntero = new System.Windows.Forms.TextBox();
+            this.txtDecimal = new System.Windows.Forms.TextBox();
+            this.lblEntero = new System.Windows.Forms.Label();
+            this.lblDecimal = new System.Windows.Forms.Label();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEntero
+            // 
+            this.txtEntero.Location = new System.Drawing.Point(51, 35);
+            this.txtEntero.Name = "txtEntero";
+            this.txtEntero.Size = new System.Drawing.Size(77, 23);
+            this.txtEntero.TabIndex = 0;
+            // 
+            // txtDecimal
+            // 
+            this.txtDecimal.Location = new System.Drawing.Point(169, 35);
+            this.txtDecimal.Name = "txtDecimal";
+            this.txtDecimal.Size = new System.Drawing.Size(77, 23);
+            this.txtDecimal.TabIndex = 1;
+            // 
+            // lblEntero
+            // 
+            this.lblEntero.AutoSize = true;
+            this.lblEntero.Location = new System.Drawing.Point(45, 68);
+            this.lblEntero.Name = "lblEntero";
+            this.lblEntero.Size = new System.Drawing.Size(41, 15);
+            this.lblEntero.TabIndex = 2;
+            this.lblEntero.Text = "Entero";
+            // 
+            // lblDecimal
+            // 
+            this.lblDecimal.AutoSize = true;
+            this.lblDecimal.Location = new System.Drawing.Point(208, 68);
+            this.lblDecimal.Name = "lblDecimal";
+            this.lblDecimal.Size = new System.Drawing.Size(50, 15);
+            this.lblDecimal.TabIndex = 3;
+            this.lblDecimal.Text = "Decimal";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(248, 105);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(189, 65);
+            this.btnCargar.TabIndex = 4;
+            this.btnCargar.Text = "button1";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(33, 105);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(189, 65);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "button1";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.lblDecimal);
+            this.Controls.Add(this.lblEntero);
+            this.Controls.Add(this.txtDecimal);
+            this.Controls.Add(this.txtEntero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEntero;
+        private System.Windows.Forms.TextBox txtDecimal;
+        private System.Windows.Forms.Label lblEntero;
+        private System.Windows.Forms.Label lblDecimal;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ unidad6/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace unidad6
+{
+    public partial class Form1 : Form
+    {
+
+        private string filePath = "datos.bin";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+       
+        private void btn_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("No se encontró el archivo de datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                int entero;
+                double decimalNumber;
+
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+                    entero = reader.ReadInt32();
+                    decimalNumber = reader.ReadDouble();
+                }
+
+                lblEntero.Text = "Entero: " + entero.ToString();
+                lblDecimal.Text = "Decimal: " + decimalNumber.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGuardar_Click_1(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+                    int entero;
+                    double decimalNumber;
+
+                    if (!int.TryParse(txtEntero.Text, out entero) || !double.TryParse(txtDecimal.Text, out decimalNumber))
+                    {
+                        MessageBox.Show("Por favor ingrese un entero y un número decimal válidos.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+                    {
+                        writer.Write(entero);
+                        writer.Write(decimalNumber);
+                    }
+
+                    MessageBox.Show("Datos guardados correctamente en el archivo binario.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+        }
+    }
+}
+
+
